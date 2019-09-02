bf9214d9dc7ff200e0e818a7680f3089f7e74abd
unknown <will.ediger@gmail.com>
2019-09-01 20:56:01 -0500

    built out authentication middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,18 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require("jsonwebtoken");
+const secrets = require("../config/secrets.js");
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const tokenHeader = req.headers.authorization;
+  if (tokenHeader) {
+    jwt.verify(tokenHeader, secrets.jwtSecret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: "You shall not pass!", error: err });
+      } else {
+        req.decodedJwt = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: "missing Authorization header" });
+  }
 };
